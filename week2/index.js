2dc6a17b9c1a6ec07b81d8e83e879dccf55a016d
dennies bor <borkiprono@gmail.com>
2024-01-23 18:56:14 -0500

    Initialize assignment 2

---
+++ week2/index.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibmRhcnVwZXRybyIsImEiOiJjbDFvbmN2djMwNXp3M2NrYjRzM3NsOHJjIn0.3n8aCZ3AcjQZ0ESVA9gTaQ';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+center: [-77.3117, 38.8315],
+zoom: 13
+});
